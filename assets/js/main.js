95dc5e9e716339b196b8c9186267234792725ff2
osama <kenoelsaht@gmail.com>
2024-03-02 19:22:43 +0200

    new-osama work

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,19 +22,6 @@
     13- portfolio Filtering and Sorting
     ----------------------------*/
 
-    const navBarToggler = document.querySelector('.navbar-toggler')
-    const navBarCollapse = document.querySelector('.navbar-collapse')
-    const clostMobileButton = document.querySelector('.close-mobile-menu')
-    const stickyNavbar = document.querySelector('.sticky-navbar')
-    const secondaryNav = document.querySelector('.secondary-nav')
-    navBarToggler.addEventListener('click' , () => {
-        navBarCollapse.classList.add('menu-opened')
-    })
-    clostMobileButton.addEventListener('click' , () => {
-        navBarCollapse.classList.remove('menu-opened')
-    })
-
-
 $(function () {
 
     "use strict";
@@ -42,6 +29,20 @@ $(function () {
     // Global variables
     var $win = $(window);
 
+    /*==========  Pre Loading   ==========*/
+    $win.on('load', function () {
+        $(".preloader").fadeOut(5000);
+        $(".preloader").remove();
+    });
+
+    /*==========   Mobile Menu   ==========*/
+    $('.navbar-toggler').on('click', function () {
+        $('.navbar-collapse').addClass('menu-opened');
+    })
+
+    $('.close-mobile-menu').on('click', function (e) {
+        $('.navbar-collapse').removeClass('menu-opened');
+    });
 
     /*==========   Sticky Navbar   ==========*/
     $win.on('scroll', function () {
@@ -132,9 +133,73 @@ $(function () {
         e.preventDefault()
     });
 
+    /*==========  Open and Close Popup   ==========*/
+    // open Popup
+    function openPopup(popupTriggerBtn, popup, addedClass, removedClass) {
+        $(popupTriggerBtn).on('click', function (e) {
+            e.preventDefault();
+            $(popup).toggleClass(addedClass, removedClass).removeClass(removedClass);
+        });
+    }
+    // Close Popup
+    function closePopup(closeBtn, popup, addedClass, removedClass) {
+        $(closeBtn).on('click', function () {
+            $(popup).removeClass(addedClass).addClass(removedClass);
+        });
+    }
+    // close popup when clicking on an other place on the Document
+    function closePopupFromOutside(popup, stopPropogationElement, popupTriggerBtn, removedClass, addedClass) {
+        $(document).on('mouseup', function (e) {
+            if (!$(stopPropogationElement).is(e.target) && !$(popupTriggerBtn).is(e.target) && $(stopPropogationElement).has(e.target).length === 0 && $(popup).has(e.target).length === 0) {
+                $(popup).removeClass(removedClass).addClass(addedClass);
+            }
+        });
+    }
+    openPopup('.action__btn-search', '.search-popup', 'active', 'inActive') // Open sidenav popup
+    closePopup('.search-popup__close', '.search-popup', 'active', 'inActive') // Close sidenav popup
+    openPopup('.action__btn-burgerMenu', '.burger-menu', 'active', 'inActive') // Open sidenav popup
+    closePopup('.burger-menu__close', '.burger-menu', 'active', 'inActive') // Close sidenav popup
+    openPopup('.action__btn-cart', '.cart-popup', 'active', 'inActive') // Open Search popup
+    closePopupFromOutside('.burger-menu', '.burger-menu__content', '.action__btn-burgerMenu', 'active', 'inActive');  // close popup when clicking on an other place on the Document
+    openPopup('.action__btn-menuPopup', '.menu-popup', 'active', 'inActive') // Open menu-popup
+    closePopup('.menu-popup__close', '.menu-popup', 'active', 'inActive') // Close menu-popup
 
+    openPopup('.open-login-popup', '#login-popup', 'active', 'inActive') // Open sidenav popup
+    closePopupFromOutside('#login-popup', '.login-popup-wrapper', '.open-login-popup', 'active', 'inActive');  // close popup when clicking on an other place on the Document
 
+    openPopup('.open-register-popup', '#register-popup', 'active', 'inActive') // Open sidenav popup
+    closePopupFromOutside('#register-popup', '.login-popup-wrapper', '.open-register-popup', 'active', 'inActive');  // close popup when clicking on an other place on the Document
+    // Open Login Popup
+    $('#go-login').on('click', function () {
+        $('#register-popup').removeClass('active').addClass('inActive');
+        $('#login-popup').removeClass('inActive').addClass('active');
+    });
+    // Open Register Popup
+    $('#go-register').on('click', function () {
+        $('#login-popup').removeClass('active').addClass('inActive');
+        $('#register-popup').removeClass('inActive').addClass('active');
+    });
 
+    /*==========  Contact Form validation  ==========*/
+    var contactForm = $("#contactForm"),
+        contactResult = $('.contact-result');
+    contactForm.validate({
+        debug: false,
+        submitHandler: function (contactForm) {
+            $(contactResult, contactForm).html('Please Wait...');
+            $.ajax({
+                type: "POST",
+                url: "assets/php/contact.php",
+                data: $(contactForm).serialize(),
+                timeout: 20000,
+                success: function (msg) {
+                    $(contactResult, contactForm).html('<div class="alert alert-success" role="alert"><strong>Thank you. We will contact you shortly.</strong></div>').delay(3000).fadeOut(2000);
+                },
+                error: $('.thanks').show()
+            });
+            return false;
+        }
+    });
 
     /*==========   Slick Carousel ==========*/
     $('.slick-carousel').slick();
@@ -158,4 +223,55 @@ $(function () {
         focusOnSelect: true,
         centerPadding: '10px'
     });
-})
+
+    /*==========  Popup Video  ==========*/
+    $('.popup-video').magnificPopup({
+        mainClass: 'mfp-fade',
+        removalDelay: 0,
+        preloader: false,
+        fixedContentPos: false,
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>',
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: '//www.youtube.com/embed/%id%?autoplay=1'
+                }
+            },
+            srcAction: 'iframe_src',
+        }
+    });
+    $('.popup-gallery-item').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+    /*==========   counterUp  ==========*/
+    $(".counter").counterUp({
+        delay: 10,
+        time: 4000
+    });
+
+    /*==========  NiceSelect Plugin  ==========*/
+    $('select').niceSelect();
+
+    /*==========   portfolio Filtering and Sorting  ==========*/
+    $("#filtered-items-wrap").mixItUp();
+    $(".portfolio-filter li a").on("click", function (e) {
+        e.preventDefault();
+    });
+});
